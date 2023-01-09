a24b9953fe92ffa7d86ba7a54de77077d905b06f
Kaio Fagundes <kaiofagundess15@gmail.com>
2023-01-09 15:51:41 -0300

    função upload de imagem

--- src/server.js
+++ src/server.js
@@ -5,11 +5,12 @@ const app = express();
 const routes = require("./routes");
 const migrationsRun = require("./database/sqlite/migrations");
 const uploadConfig = require("./configs/upload");
+const cors = require("cors");
 
 const AppError = require("./utils/AppError");
+app.use(cors());
 
 app.use(express.json());
-
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
 migrationsRun();
